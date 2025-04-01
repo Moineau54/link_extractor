78445a55e7be56852d7d1675d64ae674b4dbecc0
Moineau54 <felixjuille+github_2@proton.me>
2025-04-01 16:07:09 +0200

    fixes for screenshots

--- utils/console.py
+++ utils/console.py
@@ -35,7 +35,7 @@ class ConsoleHelper:
         self.verbose = verbose
         self.logger = logging.getLogger('link_extractor.console')
         self.app_name = "Link Extractor"
-        self.version = "2.2.1"
+        self.version = "2.2.2"
     
     def display_banner(self, verbose=False):
         """
